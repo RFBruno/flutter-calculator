642106d58b8ceabc8681afb0cb4eb7bc04123e7f
Bruno Rafael <bruno_rafael_08@hotmail.com>
2023-07-09 10:08:04 -0300

    initial project

---
+++ android/app/src/main/kotlin/br/com/rf/flutter_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.rf.flutter_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/theme_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData themeApp = ThemeData(
+// static const Color backgroundColorDark = Color(0xFF374352);
+  // static const Color backgroundColorLight = Color(0xFFe6eeff);
+  primaryColorDark: const Color(0xFF374352),
+  primaryColorLight: const Color(0xFFe6eeff),
+);
